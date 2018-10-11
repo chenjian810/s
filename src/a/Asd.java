f35bbc9fabf4eb79c393aca7e51555eeafb88853
chenjian <326354261@qq.com>
2018-10-11 23:25:41 +0800

    提交作业

---
+++ src/a/Asd.java
@@ -0,0 +1,21 @@
+package a;
+
+public class Asd {
+public int sum(int num1,int num2) {
+	int sum=num1+num2;
+	return sum;
+}
+public int jian(int num1,int num2) {
+	return num1-num2;
+	
+}
+public int cheng(int num1,int num2) {
+	return num1*num2;
+}
+public double chu(double num1,double num2) {
+	return num1/num2;
+}
+public int sum1(int num1,int num2,int num3) {
+	return num1+num2+num3;
+}
+}
